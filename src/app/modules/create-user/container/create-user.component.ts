f52f1515a09f7e1963d6386602f8e3cb8356465f
deliverymaan <deliverymaan@gmail.com>
2022-06-03 16:53:33 +0400

    form html and css and formgroup ts start

--- src/app/modules/create-user/container/create-user.component.ts
+++ src/app/modules/create-user/container/create-user.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.scss']
+  styleUrls: ['./create-user.component.scss'],
 })
 export class CreateUserComponent implements OnInit {
-
-  constructor() { }
+  userForm!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null),
+      password: new FormControl(null),
+      address: new FormControl(null),
+      gender: new FormControl(null),
+      status: new FormControl(null),
+    });
   }
-
 }
--- src/app/modules/create-user/create-user.module.ts
+++ src/app/modules/create-user/create-user.module.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateUserComponent } from './container/create-user.component';
 import { CreateUserRoutingModule } from './create-user.routing.module';
 
 @NgModule({
   declarations: [CreateUserComponent],
-  imports: [CommonModule, CreateUserRoutingModule],
+  imports: [
+    CommonModule,
+    CreateUserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class CreateUserModule {}
